998deaac77bf11f256c4b9f505a9cea5fcabb4c9
Hesam Pirzadeh <pirzadehhesam00@gmail.com>
2024-10-19 20:06:36 +0330

    better jsconfig.json

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Authenticate from '../components/templates/Authenticate'
-import Mobile from '../components/templates/Mobile'
+import Mobile from 'components/templates/Mobile'
 
 function AuthPage() {
     const [step,setStep]= useState(1)
