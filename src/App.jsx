e8f2a108164d7ef4a51b666c429aea77faf27753
Hesam Pirzadeh <pirzadehhesam00@gmail.com>
2024-10-19 20:29:13 +0330

    install react query/ queryclient/defaultsoption in config/ react query dev tool

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 
 import Router from "./router/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import defaultOptions from "config/reactQuery";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const query = new QueryClient({ defaultOptions });
   return (
-    <BrowserRouter>
-      <Router/>
-    </BrowserRouter>
+    <QueryClientProvider client={query}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
---
+++ src/config/reactQuery.js
@@ -0,0 +1,11 @@
+const defaultOptions = {
+  queries: {
+    retry: 1,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    staleTime: 60 * 1000,
+  },
+};
+
+
+export default defaultOptions
